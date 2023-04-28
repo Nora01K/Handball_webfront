83f75502abccee5f6683a91ba3493143e67f6a45
mabu2807 <77400133+mabu2807@users.noreply.github.com>
2023-04-28 18:10:43 +0200

    React-router-dom init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 
 import './App.css';
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home/Home';
+import UserHome from './pages/UserHome/UserHome';
+
 function App() {
   
   return (
-    <></>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}>
+        <Route path="test"  element={<UserHome/>} />
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    )
 }
 
 export default App;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+class Home extends React.Component {
+    state = {  } 
+    render() { 
+        return (<><h1>test</h1></>);
+    }
+}
+
+export default Home;
