94838f757c00f56c15a4c8559fa28d89821151c9
Marcel Bulling <marcel_bulling@gmx.de>
2023-08-27 19:00:53 +0200

    images

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { AppBar } from '@skeletonlabs/skeleton';
+</script>
+
+<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+	<svelte:fragment slot="lead"><img class="h-8 aspect-auto" src="handball-analyser-low-resolution-logo-white-on-transparent-background.png" alt="Logo: Handball-Analyser"/></svelte:fragment>
+    <svelte:fragment slot="trail">(actions)</svelte:fragment>
+</AppBar>
+<style lang="postcss">
+    
+
+
+</style>
