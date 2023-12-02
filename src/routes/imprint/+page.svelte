ea023c39cfb91c46e2482c7ddad0d1b1a5955d77
Nora01K <nora.klemp@t-online.de>
2023-12-02 22:02:03 +0100

    add imprint

--- src/routes/imprint/+page.svelte
+++ src/routes/imprint/+page.svelte
@@ -1,27 +1,45 @@
-<script lang=ts>
-
+<script lang="ts">
 </script>
-<main class="flex flex-col">
-    <form>
-        <div class="space-y-12">
-            <div class="font-bold text-2xl">
-                <h1>Impressum Handball Analyser</h1>
-            </div>
-            <div class="grid-row-1">
-            <div class="font-bold">
-                Marcel Bulling
-            </div>
-            <div class="font-medium">
-                Schulzenstraße 4
-            </div>
-            <div class="font-medium">
-                68259 Mannheim
-            </div>
-            <div class="font-medium">
-                Deutschland
-            </div>
-        </div>
-        </div>
-    </form>
 
+<main class="flex flex-col">
+	<form>
+		<div class="space-y-12">
+			<div class="font-bold text-2xl ml-6">
+				<h1>Impressum Handball Analyser</h1>
+			</div>
+            <div class="ml-6">Verantwortlich für den Inhalt dieser Webseite im Sinne von § 55 Abs. 2 RStV ist:
+            </div>
+            <div class=" h-full w-full p-4 border-0">
+			<div class="grid-row-1 mb-2">
+				<div class="font-bold">Marcel Bulling</div>
+				<div class="font-medium">Schulzenstraße 4</div>
+				<div class="font-medium">68259 Mannheim</div>
+				<div class="font-medium">Deutschland</div>
+			</div>
+            <div class="grid-row-1 mb-8">
+				<div class="font-bold text-xl">Kontakt</div>
+				<div class="font-medium">Telefon: +49 1753 0297539</div>
+				<div class="font-medium">E-Mail: marcel.bulling.mb@gmail.com</div>
+			</div>
+            <div class="grid-row-1 mb-2">
+                <div class="">
+                Haftung für Inhalte:
+                Als Privatperson übernehme ich keine Gewähr für die Aktualität, Vollständigkeit oder Richtigkeit der bereitgestellten Informationen auf dieser Webseite. Gemäß § 7 Abs.1 TMG bin ich für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG bin ich als Privatperson jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werde ich diese Inhalte umgehend entfernen.
+                </div>
+                <div class="">
+                Haftung für Links:
+                Meine Webseite enthält Links zu externen Webseiten Dritter, auf deren Inhalte ich keinen Einfluss habe. Daher kann ich für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werde ich derartige Links umgehend entfernen.
+                </div>
+                <div class="">
+                Urheberrecht:
+                Die durch mich erstellten Inhalte und Werke auf dieser Seite unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen meiner schriftlichen Zustimmung. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht von mir erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Solltest du trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitte ich um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werde ich derartige Inhalte umgehend entfernen.
+                </div>
+                <div class="">
+                Datenschutz:
+                Die Nutzung meiner Webseite ist ohne Angabe personenbezogener Daten möglich. Soweit auf meinen Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder E-Mail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis. Diese Daten werden ohne deine ausdrückliche Zustimmung nicht an Dritte weitergegeben.
+                </div>
+            </div>
+            </div>
+		</div>
+	</form>
 </main>
