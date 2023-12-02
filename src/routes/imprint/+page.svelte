10edf448de07e99a9cd8e00e71297411984250d8
Nora01K <nora.klemp@t-online.de>
2023-12-02 21:47:30 +0100

    initial imprint

---
+++ src/routes/imprint/+page.svelte
@@ -0,0 +1,27 @@
+<script lang=ts>
+
+</script>
+<main class="flex flex-col">
+    <form>
+        <div class="space-y-12">
+            <div class="font-bold text-2xl">
+                <h1>Impressum Handball Analyser</h1>
+            </div>
+            <div class="grid-row-1">
+            <div class="font-bold">
+                Marcel Bulling
+            </div>
+            <div class="font-medium">
+                Schulzenstraße 4
+            </div>
+            <div class="font-medium">
+                68259 Mannheim
+            </div>
+            <div class="font-medium">
+                Deutschland
+            </div>
+        </div>
+        </div>
+    </form>
+
+</main>
